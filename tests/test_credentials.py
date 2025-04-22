b47c8ffab79f68dca6118e556f30d5f71f47ae7e
gemo <kim.gemo@gmail.com>
2025-04-22 16:50:11 +0200

    reformat according to flake8

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,12 +1,16 @@
 def test_should_login(client):
-    response = client.post("/showSummary", data={"email": "john@simplylift.co"}, follow_redirects=True)
+    response = client.post(
+        "/showSummary", data={"email": "john@simplylift.co"}, follow_redirects=True
+    )
     assert response.status_code == 200
     assert "Welcome, john@simplylift.co" in response.data.decode()
+
 
 def test_should_not_login(client):
     response = client.post("/showSummary", data={"email": "john@abc.co"})
     assert response.status_code == 200
     assert "Invalid credentials." in response.data.decode()
+
 
 def test_should_not_submit(client):
     response = client.post("/showSummary", data={"email": "123"})
